ae2889da3e5b5efc298aec3843b3921db4400a5d
shehzad7714 <Shehzad.hussain7714@gmail.com>
2024-01-23 01:47:53 +0000

    Madlibs Game

---
+++ Madlibs Game.py
@@ -0,0 +1,7 @@
+colour = input("What colour: ")
+plural_noun = input(("enter the plural noun: "))
+celebrity = input("Name of celebrity: ")
+
+print(f"Roses are {colour}")
+print(f"{plural_noun} are blue")
+print(f"I love {celebrity}")
